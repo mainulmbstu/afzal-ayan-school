450f2d1375bb8935efc7a455496b2748c5e7820e
mainulmbstu <mainul1945@gmail.com>
2026-03-02 21:16:17 +0600

    mmmm

--- src/lib/components/homePage/News.js
+++ src/lib/components/homePage/News.js
@@ -2,24 +2,24 @@ import Marquee from "react-fast-marquee";
 import { getNews } from "./action";
 
 const News = async () => {
-	const data = await getNews();
-	const single = data?.success && JSON.parse(data?.single);
-	return (
-		<div>
-			<div className="flex bg-green-300">
-				<h3 className="mt-2 me-3">News:</h3>
-				<Marquee
-					behavior="scroll"
-					direction="left"
-					loop={""}
-					speed={100}
-					className="text-xl font-semibold mt-2"
-				>
-					<h3>{single ? single?.news : "No news available ||"}</h3>
-				</Marquee>
-			</div>
-		</div>
-	);
+  const data = await getNews();
+  const single = data?.success && JSON.parse(data?.single);
+  return (
+    <div>
+      <div className="flex bg-zinc-400">
+        <h3 className="mt-2 me-3">News:</h3>
+        <Marquee
+          behavior="scroll"
+          direction="left"
+          loop={""}
+          speed={100}
+          className="text-xl font-semibold mt-2"
+        >
+          <h3>{single ? single?.news : "No news available ||"}</h3>
+        </Marquee>
+      </div>
+    </div>
+  );
 };
 
 export default News;
